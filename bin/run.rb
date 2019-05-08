84709ebd0d7b340bcb8da6f8abb3e3bdc01da016
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-05-07 17:43:30 -0700

    added welcome message

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,66 @@
-require_relative '../config/environment'
+# require_relative '../config/environment'
 
-puts "hello world"
+def welcome
+  puts "WW      WW EEEEEEE LL       CCCCC   OOOOO  MM    MM EEEEEEE"
+  puts "WW      WW EE      LL      CC    C OO   OO MMM  MMM EE     "
+  puts "WW   W  WW EEEEE   LL      CC      OO   OO MM MM MM EEEEE  "
+  puts " WW WWW WW EE      LL      CC    C OO   OO MM    MM EE     "
+  puts "  WW   WW  EEEEEEE LLLLLLL  CCCCC   OOOO0  MM    MM EEEEEEE"
+  puts " "
+end
+
+def option
+  puts "1. Search for drink based on liquor type."
+  puts "2. Search for drink based on name."
+  puts "3. Search for non-alcoholic drink."
+  puts "7. Get a random drink"
+  puts ""
+  puts "8. Exit"
+  puts "====================================="
+  puts "Please enter a number to choose an option:"
+
+  options = gets.chomp.to_i
+  # def
+  
+  case options
+  when 1
+    puts "Input Liquor type:"
+
+
+  when 2
+    puts "Input drink name:"
+
+
+  when 3
+    puts "You selected non-alcoholic"
+
+
+  when 7
+    puts "You random drink is..."
+
+
+  when 8
+    puts "Bye Bye"
+  else
+    puts "\n\n!!! Invalid input. Try again!!! \n\n"
+    option
+  end
+end
+
+#running
+welcome
+option
+
+#   _
+#  {_}
+#  |(|
+#  |=|
+# /   \
+# |.--|
+# ||  |
+# ||  |    .    ' .
+# |'--|  '     \~~~/
+# '-=-' \~~~/   \_/
+#       \_/     Y
+#        Y     _|_
+# jgs    _|_
