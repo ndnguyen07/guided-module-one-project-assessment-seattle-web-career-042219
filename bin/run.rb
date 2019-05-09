238cd3de9147b3b02c738c55f786e200191181d2
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-05-09 10:25:03 -0700

    moding the run file

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,7 @@
 # require_relative '../config/environment'
 require_relative '../lib/user.rb'
 require_relative '../lib/ingredients.rb'
-require_relative '../lib/recipes.rb'
+require_relative '../lib/api_communicator.rb'
 require 'pry'
 
 def run
@@ -28,7 +28,8 @@ def welcome
                           |___/
 
 Welcome to the Cocktail Listing!
-We are going to gets some drinks going to please select from the options available below...
+We are going to gets some drinks going so please select from
+the options available below to get a drink going...
 .......
     DOC
 end
@@ -36,7 +37,8 @@ end
 ## ========== USER MAIN MENU DISPLAY ========== ##
 def option
   puts "\#1. Search for drink based on liquor type."
-  puts "\#2. Get a random drink"
+  puts "\#2. Take a chance on a random drink."
+  puts "\#3. Get the recipe when you know the name of the drink already."
   puts ""
   puts "\#0. or type 'exit' to Exit"
   puts "====================================="
@@ -51,19 +53,29 @@ def option
     #where the user can accept one to see the ingredients
     puts "Input Liquor type:"
     liquor_type = gets.chomp.to_s
-    #search_by_name(liquor_type)
+    search_by_name(liquor_type)
+    # get_ingredient
 
   when 2
-    puts "You random drink is..."
-    randomdrink = random_drink
-    puts "Save drink?"
-    User.like_drink(randomdrink)
+    puts "You random drink is the..."
+    random_drink
+    # puts "Save drink?"
+    # User.like_drink(randomdrink)
+    puts ""
+    # option
+    puts ""
+  when 3
+    puts "Know the name of the drink you want?"
+    drink_name = gets.chomp.to_s
+    getting_recipe(drink_name)
+
+
   when 0 || 'exit'
     puts "Maybe next time, friend."
 
   else
     puts "\n\n!!! Sorry but that appears to be an invalid input. Please try again!!! \n\n"
-    option
+    options
   end
 end
 
@@ -78,8 +90,30 @@ end
 
 #displays info on a random drink
 def random_drink
-  # https://www.thecocktaildb.com/api/json/v1/1/random.php
+  # result = open("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 
+  def get_json(url)
+    respond = RestClient.get(url)
+    JSON.parse(respond)
+  end
+
+  def get_ingredient
+    drink_listing = []
+    drink_data = get_json("https://www.thecocktaildb.com/api/json/v1/1/random.php")['drinks'].each {|drink, ingr|ingr}.map {|name| name.values}
+
+    drink_name  = drink_data[0][1]
+    drink_type  = drink_data[0][21...28].each do |data|
+       if data != nil || data != ""
+        # drink_listing.push(item)#item
+        data
+      end
+data
+    end
+    "Your drink is #{drink_name} and is made from #{drink_type}"
+    # "It is #{drink_type}"
+  # drink_listing
+  end
+  puts get_ingredient
   #option to save it to a list???
 end
 
